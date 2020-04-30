0f0f3aea036e36f113f8175c576370e526a7186f
Yotam Mann <yotammann@gmail.com>
2020-04-29 23:02:06 -0400

    build into dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -8,81 +8,85 @@ glob.sync(resolve(__dirname, "./examples/**/*.ts")).forEach(example => {
 	examples[name] = "./" + relative(__dirname, example);
 });
 
-const commonConfig = {
-	context: __dirname,
-	output: {
-		path: resolve(__dirname, "build"),
-		filename: "[name].js",
-	},
-	resolve: {
-		extensions: [".ts", ".js"],
-		alias: {
-			"@tonejs/gui": resolve(__dirname, "./src/gui/index.ts"),
-			// tone: resolve(__dirname, "../Tone.js/"),
-		}
-	},
-	externals: {
-		tone: "Tone"
-	},
-	module: {
-		rules: [
-			{
-				test: /\.ts$/,
-				exclude: /(node_modules)/,
-				use: {
-					loader: "ts-loader",
-				}
-			},
-			{ 
-				test: /\.scss$/, 
-				use: [
-					"to-string-loader", 
-					"css-loader", 
-					{
-						loader: "sass-loader",
-						options: {
-							sassOptions: {
-								indentedSyntax: false,
-								includePaths: ["node_modules"],
+function createCommonConfig(output) {
+	return {
+		context: __dirname,
+		output: {
+			path: resolve(__dirname, output),
+			filename: "[name].js",
+		},
+		resolve: {
+			extensions: [".ts", ".js"],
+			alias: {
+				"@tonejs/gui": resolve(__dirname, "./src/gui/index.ts"),
+				// tone: resolve(__dirname, "../Tone.js/"),
+			}
+		},
+		externals: {
+			tone: "Tone"
+		},
+		module: {
+			rules: [
+				{
+					test: /\.ts$/,
+					exclude: /(node_modules)/,
+					use: {
+						loader: "ts-loader",
+					}
+				},
+				{
+					test: /\.scss$/,
+					use: [
+						"to-string-loader",
+						"css-loader",
+						{
+							loader: "sass-loader",
+							options: {
+								sassOptions: {
+									indentedSyntax: false,
+									includePaths: ["node_modules"],
+								},
 							},
 						},
-					},
-				]
-			},
-			{ 
-				test: /\.css$/, 
-				use: [
-					"style-loader",
-					"css-loader"
-				]
-			},
-			{ 
-				test: /\.svg$/, 
-				use: [
-					"svg-inline-loader",
-				]
-			},
-		]
-	},
-};
+					]
+				},
+				{
+					test: /\.css$/,
+					use: [
+						"style-loader",
+						"css-loader"
+					]
+				},
+				{
+					test: /\.svg$/,
+					use: [
+						"svg-inline-loader",
+					]
+				},
+			]
+		},
+	};
+}
 
-module.exports = [
-	Object.assign({}, commonConfig, {
-		entry: {
-			components: "./src/index.ts"
-		}
-	}),
-	Object.assign({}, commonConfig, {
-		entry: examples,
-	}),
-	Object.assign({}, commonConfig, {
-		entry: {
-			gui: "./src/gui/index.ts"
-		},
-		output: {
-			path: resolve(__dirname, "build"),
-			filename: "tone-gui.js",
-			libraryTarget: "umd",
-		},
-	})
-];
+module.exports = env => {
+	return [
+		Object.assign({}, createCommonConfig(env.output), {
+			entry: {
+				components: "./src/index.ts"
+			}
+		}),
+		Object.assign({}, createCommonConfig(env.output), {
+			entry: examples,
+		}),
+		Object.assign({}, createCommonConfig(env.output), {
+			entry: {
+				gui: "./src/gui/index.ts"
+			},
+			output: {
+				path: resolve(__dirname, env.output),
+				filename: "tone-gui.js",
+				libraryTarget: "umd",
+			},
+		})
+	];
+};
